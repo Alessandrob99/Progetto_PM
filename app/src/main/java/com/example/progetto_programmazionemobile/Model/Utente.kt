4dc149f8d54460508a3e4a38da8c1d13144525f8
eros <S1090625@studenti.univpm.it>
2021-05-08 11:20:11 +0200

    Prova dio

--- app/src/main/java/com/example/progetto_programmazionemobile/Model/Utente.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/Model/Utente.kt
@@ -2,5 +2,5 @@ package com.example.progetto_programmazionemobile.Model
 
 class Utente {
     val stringa : String = "Ciao"
-    fun ciao() : String { return stringa+"dio bello non va un cazzo"}
+    fun ciao() : String { return stringa+"dio bello non va un cazzo, porcodioooo"}
 }
