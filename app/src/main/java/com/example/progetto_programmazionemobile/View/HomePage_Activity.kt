d590d44f23c2f8275f50e5f382ca09cdf4679c32
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-10 18:34:18 +0200

    Aggiunto abbozzo di action bar alla pagina home

--- app/src/main/java/com/example/progetto_programmazionemobile/View/HomePage_Activity.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/HomePage_Activity.kt
@@ -2,11 +2,20 @@ package com.example.progetto_programmazionemobile.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.progetto_programmazionemobile.R
 
 class HomePage_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page_)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_page_toolbar_menu, menu)
+        return true
     }
 }
