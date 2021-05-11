e5ccd9aea825d6c301b5f42639c83a679c934369
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-11 11:28:17 +0200

    Abbozzo pagina home + schermo bloccato

--- app/src/main/java/com/example/progetto_programmazionemobile/View/HomePage_Activity.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/HomePage_Activity.kt
@@ -1,5 +1,6 @@
 package com.example.progetto_programmazionemobile.View
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,7 +11,6 @@ class HomePage_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page_)
         setSupportActionBar(findViewById(R.id.toolbar))
-
 
 
     }
