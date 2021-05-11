19e650d8c09a7c65b2bbf034934d151937736be0
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-11 17:54:50 +0200

    Collegamento console FIREBASE + write DB_Handler

--- app/src/main/java/com/example/progetto_programmazionemobile/ViewModel/DB_Handler.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/ViewModel/DB_Handler.kt
@@ -1,4 +1,16 @@
 package com.example.progetto_programmazionemobile.ViewModel
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 class DB_Handler {
+
+    val db = Firebase.database
+    val myRef = db.getReference("https://prova-ee7d6-default-rtdb.europe-west1.firebasedatabase.app/message")
+
+    fun write(){
+        myRef.setValue("Hello this ia a message from the app")
+    }
 }
