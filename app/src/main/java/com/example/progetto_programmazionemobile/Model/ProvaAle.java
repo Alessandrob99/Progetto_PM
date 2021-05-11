a2cc1ac8acce9d346dbedcca12173057e45db42d
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-11 16:50:32 +0200

    Abbozzo pagina home + Nome App

--- app/src/main/java/com/example/progetto_programmazionemobile/Model/ProvaAle.java
+++
@@ -1,5 +0,0 @@
-package com.example.progetto_programmazionemobile.Model;
-
-public class ProvaAle {
-    private int x = 10;
-}
--- app/src/main/java/com/example/progetto_programmazionemobile/Model/ZZZzzzio.java
+++
@@ -1,5 +0,0 @@
-package com.example.progetto_programmazionemobile.Model;
-
-public class ZZZzzzio {
-
-}
