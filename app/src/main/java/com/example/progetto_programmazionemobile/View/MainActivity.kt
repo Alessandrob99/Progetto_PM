c136909a89809b2323e84cc5edc93975db821106
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-14 14:10:49 +0200

    Funziona.

--- app/src/main/java/com/example/progetto_programmazionemobile/View/MainActivity.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/MainActivity.kt
@@ -3,21 +3,42 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.progetto_programmazionemobile.Model.Utente
 import com.example.progetto_programmazionemobile.R
 import com.example.progetto_programmazionemobile.ViewModel.DB_Handler
+import com.google.firebase.firestore.FirebaseFirestore
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+ class MainActivity : AppCompatActivity() {
+     lateinit var user : Utente
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
         val db_conn = DB_Handler()
 
+
+
         //LETTURA
-        val userRef = db_conn.readUser("1")
+        val userReturn =  db_conn.readUser("2",object : DB_Handler.MyCallback{
+            override fun onCallback(returnValue: Utente) {
+                Toast.makeText(this@MainActivity,returnValue.nome.toString(),Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+
+
+
+
+
 
         //SCRITTURA
         //db_conn.writeUser("andre","belleggia",22,"2")
 
     }
+
 }
+
--- app/src/main/java/com/example/progetto_programmazionemobile/ViewModel/DB_Handler.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/ViewModel/DB_Handler.kt
@@ -3,11 +3,13 @@ package com.example.progetto_programmazionemobile.ViewModel
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.nfc.Tag
+import android.os.AsyncTask
 import android.util.Log
 import android.widget.Toast
 
 import com.example.progetto_programmazionemobile.Model.Utente
 import com.example.progetto_programmazionemobile.View.MainActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
@@ -15,45 +17,66 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.util.*
+import java.util.concurrent.Flow
 import javax.security.auth.callback.Callback
+import kotlin.concurrent.thread
 
 class DB_Handler {
 
     val myRef = FirebaseFirestore.getInstance()
 
-    fun writeUser(name : String, surname : String, age : Long, id : String){
-        val user = Utente(name,surname,age,id)
 
-
-        myRef.collection("users").document(id)
-                .set(user).addOnSuccessListener(OnSuccessListener {
-                    Log.d(TAG, "DocumentSnapshot successfully written!")
-                })
-                .addOnFailureListener(OnFailureListener {
-                    e -> Log.w(TAG, "Error writing document", e)
-                })
-
+    fun writeUser(name: String, surname: String, age: Long, id: String) {
+        val user = Utente(name, surname, age, id)
     }
 
-    fun readUser(id : String) : Utente {
-
-        //val utente = myRef.collection("users").document("1")
 
 
-        val docRef = myRef.collection("users").document(id)
-        val docSnap = docRef.get()
-        val docSnapRes = docSnap.result
-
-
-        val name = docSnapRes?.get("nome").toString()
-        val surname = docSnapRes?.get("cognome").toString()
-        val age = docSnapRes?.get("eta") as Long
-
-
-        val returnedUser = Utente(name,surname,age,id)
-        return returnedUser
+    fun readUser(id : String, myCallBack : MyCallback) {
+           myRef.collection("users").document(id).get().addOnSuccessListener { document ->
+               //Utente trovato
+               val data = document.data
+               val name = data?.get("nome").toString()
+               val surname = data?.get("cognome").toString()
+               val age = data?.get("eta") as Long
+               val user = Utente(name,surname,age,id)
+                myCallBack.onCallback(user)
+           }
     }
+    interface MyCallback {
+        fun onCallback(returnValue: Utente)
+    }
+
+
+
+
+
+
+
+
+    /*val docRef : DocumentReference? = myRef.collection("users").document(id)
+    if (docRef==null){
+        return null
+    }
+    val docSnap = docRef.get()
+    val docSnapRes = docSnap.result
+    Thread.sleep(2000)
+
+    val name = docSnapRes?.get("nome").toString()
+    val surname = docSnapRes?.get("cognome").toString()
+    val age = docSnapRes?.get("eta") as Long
+
+
+    val returnedUser = Utente(name,surname,age,id)
+    return returnedUser
+}
+
+     */
 }
 
 
