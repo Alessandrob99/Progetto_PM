bb6b1a0df88d55b564225716e9a00b56e6244e5d
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-19 13:03:47 +0200

    Classe Circolo e Campo

---
+++ app/src/main/java/com/example/progetto_programmazionemobile/Model/Campo.kt
@@ -0,0 +1,4 @@
+package com.example.progetto_programmazionemobile.Model
+
+class Campo {
+}
--- app/src/main/java/com/example/progetto_programmazionemobile/Model/Circolo.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/Model/Circolo.kt
@@ -1,9 +1,14 @@
 package com.example.progetto_programmazionemobile.Model
 
-class Circolo(name: String, id_club : String, town: String, surface : String)
+class Circolo(name: String, id_club : String, town: String, id_campi : ArrayList<String>,address : String, email : String,tel : String,region : String)
 {
     val nome = name
     val id = id_club
+    val regione = region
     val citta = town
-    val superficie = surface
+    val campi = id_campi
+    val indirizzo = address
+    val email = email
+    val telefono = tel
+
 }
