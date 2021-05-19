b8801948f9704e50e8f03b945aa261eb0cdae7b2
eros <S1090625@studenti.univpm.it>
2021-05-19 23:28:01 +0200

    Passaggio da Profilo a ModificaProfilo (FRAGMENTS). Fix codice ProfileFragment.kt. Cambiamento di design di qualche pagina.

---
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/EditProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.example.progetto_programmazionemobile.View
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.progetto_programmazionemobile.R
+
+class EditProfileFragment: Fragment()
+{
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_home_editprofile, container, false)
+
+        val btnConferma: Button = v.findViewById(R.id.btnConferma)
+        btnConferma.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //v!!.findNavController().navigate(R.id.editProfileFragment)
+                var fr = getFragmentManager()?.beginTransaction()
+                fr?.replace(R.id.fragment_container, ProfileFragment())
+                fr?.commit()
+            }
+        })
+        return v
+    }
+}
--- app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
@@ -2,18 +2,27 @@ package com.example.progetto_programmazionemobile.View
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.progetto_programmazionemobile.R
 
 
 class ProfileFragment : Fragment()
 {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val v : View =  inflater.inflate(R.layout.fragment_home_profile_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v: View = inflater.inflate(R.layout.fragment_home_profile_fragment, container, false)
 
+        val btnEdit: Button = v.findViewById(R.id.btnConferma)
+        btnEdit.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //v!!.findNavController().navigate(R.id.editProfileFragment)
+                var fr = getFragmentManager()?.beginTransaction()
+                fr?.replace(R.id.fragment_container, EditProfileFragment())
+                fr?.commit()
+
+            }
+        })
         return v
     }
+
 }
