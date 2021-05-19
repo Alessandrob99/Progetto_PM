abf4a3f59fbca736f9972c022042e237bae359d5
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-19 11:23:53 +0200

    Implementazione Ricerca Giocatori

--- app/src/main/java/com/example/progetto_programmazionemobile/View/RicercaGiocatori.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/RicercaGiocatori.kt
@@ -1,12 +1,19 @@
 package com.example.progetto_programmazionemobile.View
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.annotation.Nullable
+import com.example.progetto_programmazionemobile.Model.Utente
 import com.example.progetto_programmazionemobile.R
+import com.example.progetto_programmazionemobile.ViewModel.DB_Handler
 
 class RicercaGiocatori : Fragment() {
 
@@ -15,8 +22,39 @@ class RicercaGiocatori : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ricerca_giocatori, container, false)
-    }
+        val v = inflater.inflate(R.layout.fragment_ricerca_giocatori, container, false)
+        val db_conn = DB_Handler()
+        val ricercaBtn = v.findViewById<Button>(R.id.btnRicerca)
+        var queryUser = v.findViewById<EditText>(R.id.Ricerca)
+        ricercaBtn.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if (v != null) {
+                    if(TextUtils.isEmpty(queryUser.getText().toString())){
+                        Toast.makeText(this@RicercaGiocatori.context, "Inserire qualcosa", Toast.LENGTH_SHORT).show()
+                    }else{
 
+                        val query = queryUser.text.toString()
+                        if (TextUtils.equals(query,"Ricerca")) {
+                            Toast.makeText(this@RicercaGiocatori.context, "Inserire qualcosa", Toast.LENGTH_SHORT).show()
+
+                        } else {
+                            db_conn.SearchUsers(query, object : DB_Handler.MyCallbackFoundUsers {
+                                override fun onCallback(returnUser: ArrayList<Utente>) {
+                                    //INTENT TO ACTIVITY FOR RESULTS
+                                    val intent = Intent(this@RicercaGiocatori.context, SearchResult::class.java)
+                                    intent.putExtra("usersList", returnUser)
+                                    startActivity(intent)
+                                }
+                            })
+
+                        }
+                    }
+
+                }
+
+            }
+        })
+        return v
+    }
 }
+
