966de1c1f87051618f6cbcd850948133ea9e6b65
eros <S1090625@studenti.univpm.it>
2021-05-20 12:55:02 +0200

    Fragment Profile con Hide/Show Password.

--- app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.example.progetto_programmazionemobile.View
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.view.*
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.progetto_programmazionemobile.R
 
 
 class ProfileFragment : Fragment()
 {
+    private lateinit var password : EditText
+    private lateinit var showpass : ImageView
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v: View = inflater.inflate(R.layout.fragment_home_profile_fragment, container, false)
 
@@ -25,7 +32,5 @@ class ProfileFragment : Fragment()
         return v
     }
 
-
-
-
 }
+
