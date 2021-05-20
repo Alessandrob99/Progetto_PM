d1fcac0af0d41f5c93bd6aff60c05beca9e03662
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-20 12:52:06 +0200

    PopUp Logout

--- app/src/main/java/com/example/progetto_programmazionemobile/View/LogoutPopUp.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/LogoutPopUp.kt
@@ -1,14 +1,10 @@
 package com.example.progetto_programmazionemobile.View
 
-import android.animation.ArgbEvaluator
-import android.animation.ValueAnimator
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.core.graphics.ColorUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.example.progetto_programmazionemobile.R
 
 class LogoutPopUp : AppCompatActivity() {
