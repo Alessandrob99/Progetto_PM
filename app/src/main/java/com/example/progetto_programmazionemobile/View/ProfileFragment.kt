9adddc4f7d034015c462c905ee09c5fcacdc9fcd
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-05-25 19:12:58 +0200

    Modifica Grafica InfoFragment

--- app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/ProfileFragment.kt
@@ -6,10 +6,12 @@ import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.progetto_programmazionemobile.R
+import com.example.progetto_programmazionemobile.ViewModel.Auth_Handler
 
 
 class ProfileFragment : Fragment()
@@ -21,6 +23,8 @@ class ProfileFragment : Fragment()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v: View = inflater.inflate(R.layout.fragment_home_profile_fragment, container, false)
 
+        val userText = v.findViewById<TextView>(R.id.txtUsername)
+        userText.setText(Auth_Handler.CURRENT_USER?.username)
 
         val btnEdit: Button = v.findViewById(R.id.btnConferma)
         btnEdit.setOnClickListener(object : View.OnClickListener {
--- app/src/main/java/com/example/progetto_programmazionemobile/View/infoFragment.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/View/infoFragment.kt
@@ -6,16 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.findNavController
 import com.example.progetto_programmazionemobile.R
+import com.example.progetto_programmazionemobile.ViewModel.Auth_Handler
 
 class infoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_info, container, false)
 
+        val welcomeText = v.findViewById<TextView>(R.id.welcome)
+        welcomeText.setText("Benvenuto "+ (Auth_Handler.CURRENT_USER!!.nome!!.capitalize())+"!")
+
         val btnUnisciti : Button = v.findViewById(R.id.btnUniscitiPrenotazione)
+
 
 
 
