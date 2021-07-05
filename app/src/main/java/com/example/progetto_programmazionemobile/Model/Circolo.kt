96685ecb08f53e1eac9f3a6bddedeb3ed9240509
Alessandrob99 <alessandrobedetta941@gmail.com>
2021-07-05 10:39:42 +0200

    -filtri

--- app/src/main/java/com/example/progetto_programmazionemobile/Model/Circolo.kt
+++ app/src/main/java/com/example/progetto_programmazionemobile/Model/Circolo.kt
@@ -21,60 +21,64 @@ class Circolo(id : Long,name: String, email : String,tel : String, docce : Boole
     }
     companion object{
 
-        fun filterClubs(campiPerSport : ArrayList<Campo>,myLoc : Location,range : Float?,riscaldamento : Boolean,docce : Boolean,coperto : Boolean,superficie : String,prezzoMax : Float?,myCallbackClubs1: MyCallbackClubs) {
+        fun filterClubs(campiPerSportDaFiltrare : ArrayList<Campo>,myLoc : Location,range : Float?,riscaldamento : Boolean,docce : Boolean,coperto : Boolean,superficie : String,prezzoMax : Float?,myCallbackClubs1: MyCallbackClubs) {
 
             var clubs = ArrayList<Circolo>()   // Lo andiamo a riempire man mano che scorriamo i filtri
+
+            var campiPerSportFiltrati = ArrayList<Campo>()    // Lista dei campi SOPRAVVISSUTI al filtraggio
+            for(campo in campiPerSportDaFiltrare){ campiPerSportFiltrati.add(campo) }
 
             //Controllo sulla distanza massima (Per primo cosi evitiamo di fare controlli inutili)
             if (range != null) {
                 DB_Handler_Clubs.getAllClubsInRange(object : DB_Handler_Clubs.MyCallbackClubs {
                     override fun onCallback(returnedClubs: ArrayList<Circolo>?) {
-                        //Per ogni campo vedo se si trova in uno dei circoli nel range
-                        var clubsIDs = ArrayList<String>()
-                        //For di filtraggio per gli altri paramentri
-                        var campiIterator = campiPerSport.iterator()
-                        while(campiIterator.hasNext()){
-                            val campo = campiIterator.next()
-                            //Controllo sulla superficie
-                            if(superficie!="Tutte"){
-                                if(campo.superficie.equals(superficie)){
-                                    campiIterator.remove()
-                                 }
-                            }
-                            //Controllo sulla presenza di riscaldamento
-                            if(riscaldamento){
-                                if(campo.riscaldamento){
-                                    campiIterator.remove()
+                        if(returnedClubs!=null){
+                            //Per ogni campo vedo se si trova in uno dei circoli nel range
+                            var clubsIDs = ArrayList<String>()
+                            clubs = returnedClubs!!
+                            //For di filtraggio per gli altri paramentri
+                            for (campo in campiPerSportDaFiltrare) {
+                                //Controllo sulla superficie
+                                if (superficie != "Tutte") {
+                                    if (!campo.superficie.equals(superficie)) {
+                                        campiPerSportFiltrati.remove(campo)
+                                    }
+                                }
+                                //Controllo sulla presenza di riscaldamento
+                                if (riscaldamento) {
+                                    if (!campo.riscaldamento) {
+                                        campiPerSportDaFiltrare.remove(campo)
+                                    }
+                                }
+                                //Controllo sul prezzo massimo
+                                if (prezzoMax != null) {
+                                    if (campo.prezzo_ora > prezzoMax) {
+                                        campiPerSportFiltrati.remove(campo)
+                                    }
                                 }
                             }
-                            //Controllo sul prezzo massimo
-                            if(prezzoMax!=null){
-                                if(campo.prezzo_ora.toFloat()>prezzoMax){
-                                    campiIterator.remove()
-
+                            //Controllo sulla presenza di docce/spogliatoi
+                            if (docce) {
+                                if (returnedClubs != null) {
+                                    for (club in returnedClubs) {
+                                        if (club.docce) {
+                                            clubs.remove(club)
+                                        }
+                                    }
                                 }
                             }
-                        }
-                        //Controllo sulla presenza di docce/spogliatoi
-                        if(docce){
-                            for(i in clubs.indices.reversed()){
-                                if(!clubs[i].docce){
-                                    clubs.removeAt(i)
-                                }
+                            //Troviamo i circoli (in range) corrispondenti ai campi filtrati
+                            for (court in campiPerSportFiltrati) {
+                                clubsIDs.add(court.id_club.toString())
                             }
-                        }
-                       //Troviamo i circoli (in range) corrispondenti ai campi filtrati
-                        for (court in campiPerSport) {
-                            clubsIDs.add(court.id_club.toString())
-                        }
-                        if (returnedClubs != null) {
                             for (record in returnedClubs) {
                                 if (clubsIDs.contains(record.id.toString())) {
-                                    clubs.add(record)
+                                    clubs.remove(record)
                                 }
                             }
+
+                            myCallbackClubs1.onCallback(clubs)
                         }
-                        myCallbackClubs1.onCallback(clubs)
                     }
                 }, myLoc, range)
             } else {
@@ -82,23 +86,23 @@ class Circolo(id : Long,name: String, email : String,tel : String, docce : Boole
                     override fun onCallback(returnedClubs: ArrayList<Circolo>?) {
                         var clubsIDs = ArrayList<String>()
                         //For di filtraggio per gli altri paramentri
-                        for (i in campiPerSport.indices) {
+                        for (i in campiPerSportDaFiltrare.indices) {
                             //Controllo sulla superficie
                             if(superficie!="Tutte"){
-                                if(!campiPerSport[i].superficie.equals(superficie)){
-                                    campiPerSport.removeAt(i)
+                                if(!campiPerSportDaFiltrare[i].superficie.equals(superficie)){
+                                    campiPerSportDaFiltrare.removeAt(i)
                                 }
                             }
                             //Controllo sulla presenza di riscaldamento
                             if(riscaldamento){
-                                if(!campiPerSport[i].riscaldamento){
-                                    campiPerSport.removeAt(i)
+                                if(!campiPerSportDaFiltrare[i].riscaldamento){
+                                    campiPerSportDaFiltrare.removeAt(i)
                                 }
                             }
                             //Controllo sul prezzo massimo
                             if(prezzoMax!=null){
-                                if(campiPerSport[i].prezzo_ora>prezzoMax){
-                                    campiPerSport.removeAt(i)
+                                if(campiPerSportDaFiltrare[i].prezzo_ora>prezzoMax){
+                                    campiPerSportDaFiltrare.removeAt(i)
                                 }
                             }
                         }
@@ -111,7 +115,7 @@ class Circolo(id : Long,name: String, email : String,tel : String, docce : Boole
                             }
                         }
                         //Troviamo i circoli corrispondenti ai campi filtrati
-                        for (court in campiPerSport) {
+                        for (court in campiPerSportFiltrati) {
                             clubsIDs.add(court.id_club.toString())
                         }
                         if (returnedClubs != null) {
